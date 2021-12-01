183dc16ac7ed0c68161c4133a9016034b1c2dd39
Ryan <72553021+MaEdAlpha@users.noreply.github.com>
2021-12-01 15:33:03 +0700

    Added css styling

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,5 +1,15 @@
-function ExpenseItem(){
-    return <h2>ExpenseItem Component Works!</h2>;
+import './ExpenseItem.css';
+
+function ExpenseItem() {
+  return (
+    <div className="expense-item ">
+      <div> January 1st 1970</div>
+      <div className="expense-item__description">
+        <h2>Computer</h2>
+      </div>
+      <div className="expense-item__price" >$1,000,000</div>
+    </div>
+  );
 }
 
 export default ExpenseItem;
