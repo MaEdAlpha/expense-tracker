636bca1be32dce11f9380a7fb38215ad721eb0da
Ryan <72553021+MaEdAlpha@users.noreply.github.com>
2021-12-07 09:49:05 +0700

    Form Value Input
    Added Form Value Input

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,23 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './ExpenseForm.css';
 
 const ExpenseForm = () =>{
+    const [titleInput, setTitleInputValue] = useState('');
+    const [amountInput, setAmountInputValue] = useState('0');
+    const [dateInput, setDateInputValue] = useState('');
+
+    const titleChangeHandler = (event) => {
+        setTitleInputValue(event.target.value); //stores in some variable
+    };
+
+    const amountChangeHandler = (event) => {
+        setAmountInputValue(event.target.value);
+    };
+
+    const dateChangeHandler = (event) => {
+        setDateInputValue(event.target.value);
+    };
+
+    const logValues = () => {
+        console.log(titleInput);
+        console.log(amountInput);
+        console.log(dateInput);
+    }
+
     return <form>
         <div className="new-expense__controls">
             <div className="new-expense__control">
                 <label> New Item</label>
-                <input type='text' />
+                <input type='text' onChange={titleChangeHandler}/>
             </div>
             <div className="new-expense__control">
                 <label> Amount</label>
-                <input type='number' min="0.01" step="1.0" />
+                <input type='number' min="0" max="100000" step="1.0" onChange={amountChangeHandler} />
             </div>
             <div className="new-expense__control">
                 <label> Date</label>
-                <input type='date' min="2020-01-01" max="2022-12-31" />
+                <input type='date' min="2020-01-01" max="2022-12-31" onChange={dateChangeHandler}/>
             </div>
             <div className="new-expense__actions">
-                <button type='submit'>Add Expense</button>
+                <button  onClick={logValues}>Add Expense</button>
             </div>
         </div>
     </form>
